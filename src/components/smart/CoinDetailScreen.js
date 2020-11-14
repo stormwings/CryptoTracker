2a2521f23cf8e6b3bff40b58bc3a77efe5e72b7e
Mariano Villarreal <marianov994@gmail.com>
2020-11-14 20:40:59 -0300

    add button to detail screen coin

--- src/components/smart/CoinDetailScreen.js
+++ src/components/smart/CoinDetailScreen.js
@@ -1,12 +1,30 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Pressable } from 'react-native';
 
-const CoinDetailScreen = () => {
+const CoinDetailScreen = (props) => {
+  const { coin } = props.route.params;
+  console.log(coin);
   return (
     <View>
       <Text>This is a coin detail</Text>
+      <Pressable style={styles.btn} onPress={() => true}>
+        <Text style={styles.btnText}>Ir a Detail</Text>
+      </Pressable>
     </View>
   );
+};
+
+const styles = {
+  btn: {
+    padding: 8,
+    backgroundColor: '#0E74F6',
+    borderRadius: 8,
+    margin: 16,
+  },
+  btnText: {
+    color: '#fff',
+    textAlign: 'center',
+  },
 };
 
 export default CoinDetailScreen;
