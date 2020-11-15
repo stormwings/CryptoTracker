348a3d8e431a2698f1169e949458fa32003b840f
Mariano Villarreal <marianov994@gmail.com>
2020-11-14 21:22:43 -0300

    CoinDetailScreen remove button and show data

--- src/components/smart/CoinDetailScreen.js
+++ src/components/smart/CoinDetailScreen.js
@@ -1,30 +1,132 @@
 import React from 'react';
-import { View, Text, Pressable } from 'react-native';
+import { View, Text, StyleSheet, SectionList, Image } from 'react-native';
 
 const CoinDetailScreen = (props) => {
   const { coin } = props.route.params;
-  console.log(coin);
+
+  const getSymbolIcon = (name) => {
+    if (name) {
+      const symbol = name.toLowerCase().replace(' ', '-');
+
+      return `https://c1.coinlore.com/img/25x25/${symbol}.png`;
+    }
+  };
+
+  const getSections = (item) => {
+    const sections = [
+      {
+        title: 'Market cap',
+        data: [item.market_cap_usd],
+      },
+      {
+        title: 'Volume 24h',
+        data: [item.volume24],
+      },
+      {
+        title: 'Change 24h',
+        data: [item.percent_change_24h],
+      },
+    ];
+
+    return sections;
+  };
+
   return (
-    <View>
-      <Text>This is a coin detail</Text>
-      <Pressable style={styles.btn} onPress={() => true}>
-        <Text style={styles.btnText}>Ir a Detail</Text>
-      </Pressable>
+    <View style={styles.container}>
+      <View style={styles.subHeader}>
+        <Image
+          style={styles.iconImg}
+          source={{ uri: getSymbolIcon(coin.name) }}
+        />
+        <Text style={styles.titleText}>{coin.name}</Text>
+      </View>
+
+      <SectionList
+        style={styles.section}
+        sections={getSections(coin)}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <View style={styles.sectionItem}>
+            <Text style={styles.itemText}>{item}</Text>
+          </View>
+        )}
+        renderSectionHeader={({ section }) => (
+          <View style={styles.sectionHeader}>
+            <Text style={styles.sectionText}>{section.title}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 };
 
-const styles = {
-  btn: {
-    padding: 8,
-    backgroundColor: '#0E74F6',
-    borderRadius: 8,
-    margin: 16,
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
   },
-  btnText: {
+  row: {
+    flexDirection: 'row',
+  },
+  subHeader: {
+    backgroundColor: 'rgba(0, 0, 0, 0.1)',
+    padding: 16,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  titleText: {
+    fontSize: 16,
+    fontWeight: 'bold',
     color: '#fff',
-    textAlign: 'center',
+    marginLeft: 8,
   },
-};
+  iconImg: {
+    width: 25,
+    height: 25,
+  },
+  section: {
+    maxHeight: 220,
+  },
+  list: {
+    maxHeight: 100,
+    paddingLeft: 16,
+  },
+  sectionHeader: {
+    backgroundColor: 'rgba(0,0,0, 0.2)',
+    padding: 8,
+  },
+  sectionItem: {
+    padding: 8,
+  },
+  itemText: {
+    color: 'white',
+    fontSize: 14,
+  },
+  sectionText: {
+    color: 'white',
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+  marketsTitle: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 16,
+    marginLeft: 16,
+  },
+  btnFavorite: {
+    padding: 8,
+    borderRadius: 8,
+  },
+  btnFavoriteText: {
+    color: 'white',
+  },
+  btnFavoriteAdd: {
+    backgroundColor: 'yellow',
+  },
+  btnFavoriteRemove: {
+    backgroundColor: 'cyan',
+  },
+});
 
 export default CoinDetailScreen;
