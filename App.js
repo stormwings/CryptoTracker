9c6e9938e3731925afa6e18d231a5aefa8ec6487
Mariano Villarreal <marianov994@gmail.com>
2020-11-19 02:13:09 -0300

    bottom tab coin, configs and styles

--- App.js
+++ App.js
@@ -1,13 +1,43 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import { Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import CoinsStack from './src/components/view/CoinsStack';
+
+const Tabs = createBottomTabNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <CoinsStack />
+      <Tabs.Navigator tabBarOptions={styles.tabBar}>
+        <Tabs.Screen
+          name="Coins"
+          component={CoinsStack}
+          options={tabsConfigs.coins}
+        />
+      </Tabs.Navigator>
     </NavigationContainer>
   );
+};
+
+const tabsConfigs = {
+  coins: {
+    tabBarIcon: ({ size, color }) => (
+      <Image
+        style={{ tintColor: color, width: size, height: size }}
+        source={require('./src/assets/bank.png')}
+      />
+    ),
+  },
+};
+
+const styles = {
+  tabBar: {
+    tintColor: '#fefefe',
+    style: {
+      backgroundColor: 'black',
+    },
+  },
 };
 
 export default App;
