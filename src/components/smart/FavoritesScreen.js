2497c0199722a17b54f4c81800ca8f75b92307e2
Mariano Villarreal <marianov994@gmail.com>
2020-11-19 17:22:00 -0300

    FavoriteScreen completed

--- src/components/smart/FavoritesScreen.js
+++ src/components/smart/FavoritesScreen.js
@@ -1,12 +1,55 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, StyleSheet, FlatList } from 'react-native';
+import CoinListItem from './../dumb/CoinListItem';
 import EmptyState from './EmptyState';
+import Storage from './../../libs/storage';
 
-const FavoritesScreen = () => (
-  <View style={styles.container}>
-    <EmptyState />
-  </View>
-);
+const FavoritesScreen = ({ navigation }) => {
+  const [favorites, setFavorites] = useState([]);
+
+  const getFavorites = async () => {
+    try {
+      const allKeys = await Storage.instance.getAllkeys();
+
+      const keys = allKeys.filter((key) => key.includes('favorite-'));
+
+      const favs = await Storage.instance.multiGet(keys);
+
+      const receivedFavorites = favs.map((fav) => JSON.parse(fav[1]));
+
+      setFavorites(receivedFavorites);
+    } catch (err) {
+      console.log('get favorites err', err);
+    }
+  };
+
+  const handlePress = (coin) => {
+    navigation.navigate('CoinDetailScreen', { coin });
+  };
+
+  useEffect(() => {
+    const unsuscribe = navigation.addListener('focus', () => {
+      getFavorites();
+    });
+
+    return unsuscribe;
+  }, [navigation]);
+
+  return (
+    <View style={styles.container}>
+      {favorites.length === 0 && <EmptyState />}
+
+      {favorites.length > 0 && (
+        <FlatList
+          data={favorites}
+          renderItem={({ item }) => (
+            <CoinListItem item={item} onPress={() => handlePress(item)} />
+          )}
+        />
+      )}
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
