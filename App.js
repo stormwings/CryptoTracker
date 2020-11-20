753764e8dd7a9d78304aa85d02623997d1d37456
Mariano Villarreal <marianov994@gmail.com>
2020-11-20 01:40:03 -0300

    redux and redux-persist connection with app

--- App.js
+++ App.js
@@ -2,28 +2,43 @@ import React from 'react';
 import { Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { PersistGate } from 'redux-persist/integration/react';
+import { Provider } from 'react-redux';
 
 import CoinsStack from './src/components/view/CoinsStack';
 import FavoritesStack from './src/components/view/FavoritesStack';
 
+import store from './src/redux/store';
+export let persistor = store();
+
 const Tabs = createBottomTabNavigator();
+
+const ReduxContainer = (props) => (
+  <Provider store={persistor.store}>
+    <PersistGate loading={null} persistor={persistor.persistor}>
+      {props.children}
+    </PersistGate>
+  </Provider>
+);
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Tabs.Navigator tabBarOptions={styles.tabBar}>
-        <Tabs.Screen
-          name="Coins"
-          component={CoinsStack}
-          options={tabsConfigs.coins}
-        />
-        <Tabs.Screen
-          name="Favorites"
-          component={FavoritesStack}
-          options={tabsConfigs.favorites}
-        />
-      </Tabs.Navigator>
-    </NavigationContainer>
+    <ReduxContainer>
+      <NavigationContainer>
+        <Tabs.Navigator tabBarOptions={styles.tabBar}>
+          <Tabs.Screen
+            name="Coins"
+            component={CoinsStack}
+            options={tabsConfigs.coins}
+          />
+          <Tabs.Screen
+            name="Favorites"
+            component={FavoritesStack}
+            options={tabsConfigs.favorites}
+          />
+        </Tabs.Navigator>
+      </NavigationContainer>
+    </ReduxContainer>
   );
 };
 
