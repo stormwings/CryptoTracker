19fb463e9ac2f59aafa84952e95dc63c0fa00112
Mariano Villarreal <marianov994@gmail.com>
2020-11-24 22:39:12 -0300

    add new url to get coin prices

--- src/redux/urls.js
+++ src/redux/urls.js
@@ -1,3 +1,8 @@
 const baseUrl = 'localhost:8000';
 
+const coinGecko = 'https://api.coingecko.com/api/v3/';
+
 export const urlFavorites = `${baseUrl}/favorites`;
+
+export const urlCryptoPriceChart = (crypto) =>
+  `${coinGecko}coins/${crypto}/market_chart?vs_currency=usd&days=5&interval=daily`;
